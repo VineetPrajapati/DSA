6514c3b6e4c1b1e723e1ae3e44fc747d04e7c478
Vineet <infinite2202@gmail.com>
2024-09-18 17:51:02 +0530

    kth smallest term

---
+++ Array/kthSmallest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int kthsmallest(int arr[], int n, int k)
+{
+    sort(arr, arr + n);
+
+    return arr[k - 1];
+}
+
+int main()
+{
+    int arr[] = {7, 10, 4, 3, 20, 15};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Kth smallest term is : " << kthsmallest(arr, size, 3);
+
+    return 0;
+}
